93e4fde01811e8261869c109f2f18ff1a234dc38
Robbin Schrijver <robbinschrijver07@gmail.com>
2025-10-06 11:33:35 +0200

    Created a popup with styling for an admin to log in

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,18 +3,25 @@ import "../styles/App.css";
 import "../styles/header.css";
 import logo from "../assets/img/logo.png";
 import axios from "axios";
-// import PopupContent from "./popup";
+import PopupContent from "./popup";
 
 function Header() {
   const [showPopup, setShowPopup] = useState(false);
+
+  const handleOpenPopup = () => setShowPopup(true);
+  const handleClosePopup = () => setShowPopup(false);
 
   return (
     <>
       <header>
         <img src={logo} alt="Cool frog" />
 
-        <button id="login">Log in</button>
+        <button id="login" onClick={handleOpenPopup}>
+          Log in
+        </button>
       </header>
+
+      {showPopup && <PopupContent onClose={handleClosePopup} />}
     </>
   );
 }
---
+++ client/src/components/popup.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import "../styles/App.css";
+import "../styles/header.css";
+import logo from "../assets/img/logo.png";
+import axios from "axios";
+
+function PopupContent({ onClose }) {
+  return (
+    <>
+      <div className="overlay" onClick={onClose}></div>
+      {/* prevents closing while inside the popup */}
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <h3>Log in</h3>
+        <form>
+          <input type="text" placeholder="Username" required />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            minLength={6}
+            required
+          />
+          <br />
+          <button type="submit">Inloggen</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default PopupContent;
